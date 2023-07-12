2bf98bdcca6e57e8ae6558bb69f7dd7ca66ddea0
David Raj <138006322+DavidChennaiPP@users.noreply.github.com>
2023-07-12 23:25:17 +0400

    second commit

--- script.js
+++ script.js
@@ -1,71 +1,101 @@
-let currentPlayer = "X";
-let gameEnded = false;
-
-const placeSymbol = (cellIndex) => {
-  if (gameEnded) return;
-
-  const cell = document.getElementsByClassName("cell")[cellIndex];
-  if (cell.textContent === "") {
-    cell.textContent = currentPlayer;
-    cell.classList.add(currentPlayer);
-
-    if (checkWin()) {
-      endGame(currentPlayer + " wins!");
-    } else if (checkDraw()) {
-      endGame("It's a draw!");
+let btnRef = document.querySelectorAll(".button-option");
+let popupRef = document.querySelector(".popup");
+let newgameBtn = document.getElementById("new-game");
+let restartBtn = document.getElementById("restart");
+let msgRef = document.getElementById("message");
+//Winning Pattern Array
+let winningPattern = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [2, 5, 8],
+  [6, 7, 8],
+  [3, 4, 5],
+  [1, 4, 7],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+//Player 'X' plays first
+let xTurn = true;
+let count = 0;
+//Disable All Buttons
+const disableButtons = () => {
+  btnRef.forEach((element) => (element.disabled = true));
+  //enable popup
+  popupRef.classList.remove("hide");
+};
+//Enable all buttons (For New Game and Restart)
+const enableButtons = () => {
+  btnRef.forEach((element) => {
+    element.innerText = "";
+    element.disabled = false;
+  });
+  //disable popup
+  popupRef.classList.add("hide");
+};
+//This function is executed when a player wins
+const winFunction = (letter) => {
+  disableButtons();
+  if (letter == "X") {
+    msgRef.innerHTML = "&#x1F389; <br> 'X' Wins";
+  } else {
+    msgRef.innerHTML = "&#x1F389; <br> 'O' Wins";
+  }
+};
+//Function for draw
+const drawFunction = () => {
+  disableButtons();
+  msgRef.innerHTML = "&#x1F60E; <br> It's a Draw";
+};
+//New Game
+newgameBtn.addEventListener("click", () => {
+  count = 0;
+  enableButtons();
+});
+restartBtn.addEventListener("click", () => {
+  count = 0;
+  enableButtons();
+});
+//Win Logic
+const winChecker = () => {
+  //Loop through all win patterns
+  for (let i of winningPattern) {
+    let [element1, element2, element3] = [
+      btnRef[i[0]].innerText,
+      btnRef[i[1]].innerText,
+      btnRef[i[2]].innerText,
+    ];
+    //Check if elements are filled
+    //If 3 empty elements are same and would give win as would
+    if (element1 != "" && (element2 != "") & (element3 != "")) {
+      if (element1 == element2 && element2 == element3) {
+        //If all 3 buttons have same values then pass the value to winFunction
+        winFunction(element1);
+      }
+    }
+  }
+};
+//Display X/O on click
+btnRef.forEach((element) => {
+  element.addEventListener("click", () => {
+    if (xTurn) {
+      xTurn = false;
+      //Display X
+      element.innerText = "X";
+      element.disabled = true;
     } else {
-      currentPlayer = currentPlayer === "X" ? "O" : "X";
+      xTurn = true;
+      //Display Y
+      element.innerText = "O";
+      element.disabled = true;
     }
-  }
-};
-
-const checkWin = () => {
-  const winningCombinations = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-
-  const cells = document.getElementsByClassName("cell");
-  for (let combination of winningCombinations) {
-    const [a, b, c] = combination;
-    if (
-      cells[a].textContent === cells[b].textContent &&
-      cells[b].textContent === cells[c].textContent &&
-      cells[a].textContent !== ""
-    ) {
-      return true;
+    //Increment count on each click
+    count += 1;
+    if (count == 9) {
+      drawFunction();
     }
-  }
-  return false;
-};
-
-const checkDraw = () => {
-  const cells = document.getElementsByClassName("cell");
-  for (let cell of cells) {
-    if (cell.textContent === "") {
-      return false;
-    }
-  }
-  return true;
-};
-
-const endGame = (message) => {
-  gameEnded = true;
-  alert(message);
-};
-
-const resetBoard = () => {
-  const cells = document.getElementsByClassName("cell");
-  for (let cell of cells) {
-    cell.textContent = "";
-    cell.classList.remove("X", "O");
-  }
-  currentPlayer = "X";
-  gameEnded = false;
-};
+    //Check for win on every click
+    winChecker();
+  });
+});
+//Enable Buttons and disable popup on page load
+window.onload = enableButtons;
